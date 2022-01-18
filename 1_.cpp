2104e1b0f13dd6226b8d3c782d6ac33e8d531552
3uby <bsa0530@gmail.com>
2022-01-19 08:26:09 +0900

    d

--- 1_.cpp
+++ 1_.cpp
@@ -5,31 +5,19 @@
 
 using namespace std;
 
-void solve(){
-    int n, cnt = 0;
-    cin >> n;   
-    if(n % 3){
-        cnt += n / 3; 
-        n = n % 3;
-    } 
-    if(n % 2){
-        cnt += n /2;
-        n = n % 2;
-    } 
-    if( n == 2){
-        cnt += 1;
-        n = n -1;
-    }
-    cout << n << endl;
-    cout << cnt << endl;
+int solve(int){
+int answers;
     
+    return answers;
 }
 
 int main(){
+int result;
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     //cout << "Hello World" << endl;
     solve();
+    cout << result;
     return 0;
 }
